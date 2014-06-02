7a7bde85573e6a02142de8d12a24ace0ad83e5eb
naileditdesign <adam@naileditdesign.com>
2014-06-02 08:14:01 +0100

    Added Slick Slider to Replace Orbit
    Orbit has gone Bye Bye in F5

--- themes/nailedit_boilerplate/footer.php
+++ themes/nailedit_boilerplate/footer.php
@@ -8,6 +8,9 @@
 <!-- // Javascripts -->
 
 <script src="<?php bloginfo('template_url'); ?>/js/foundation.min.js"></script>
+<!-- // Slicker -->
+<script src="<?php bloginfo('template_url'); ?>/js/slick.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/slick-scripts.js"></script>
 <!-- // Cookie Bar NOM NOM-->
 <script src="<?php bloginfo('template_url'); ?>/js/jquery.cookiebar.js"></script>
 <script src="<?php bloginfo('template_url'); ?>/js/extend.js"></script>	
